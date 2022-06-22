8a1af385a4546095a33a0aa2a784f9e9fbbb0ea5
Park323 <park32323@gmail.com>
2022-06-22 16:02:18 +0900

    enable dim control Now Encoder front-end dimension control is available - enable audio only & video only mode - linear projection attached between front-end and back-end of Encoder

--- avsr/model_builder.py
+++ avsr/model_builder.py
@@ -13,7 +13,9 @@ def count_parameters(model):
 def build_model(
     vocab_size : int,
     pad_id : int,
-    architecture:str = 'default',
+    architecture : str = 'default',
+    loss_fn : str = 'default',
+    front_dim=None,
     encoder_n_layer=None, 
     encoder_d_model=None,
     encoder_n_head=None, 
@@ -24,19 +26,57 @@ def build_model(
     decoder_n_head=None, 
     decoder_ff_dim=None, 
     decoder_dropout_p=None,
+    rank = 0,
 ):
-    if architecture=='default': architecture = 'hybrid'
+    if architecture=='default': architecture = 'audio_visual'
+    if loss_fn=='default': architecture = 'hybrid'
     
-    if architecture == 'hybrid':
+    if rank==0 or rank is None:
+        print(f"Build {loss_fn} {architecture} model...")
+    
+    
+    # Define Model
+    if loss_fn == 'hybrid':
         model = HybridModel(vocab_size=vocab_size, pad_id=pad_id)
-        model.embedder = nn.Linear(vocab_size, decoder_d_model)
+    elif loss_fn == 'att' or architecture=='attention':
+        model = AttentionModel(vocab_size=vocab_size, pad_id=pad_id)
+    elif loss_fn == 'ctc':
+        model = CTCModel(vocab_size=vocab_size, pad_id=pad_id)
+
+    # Define Embedder
+    model.embedder = nn.Linear(vocab_size, decoder_d_model)
+
+    # Define Encoder
+    if architecture == 'audio_visual':
         model.encoder = FusionConformerEncoder(
+            front_dim = front_dim,
             encoder_n_layer=encoder_n_layer, 
             encoder_d_model=encoder_d_model,
             encoder_n_head=encoder_n_head, 
             encoder_ff_dim=encoder_ff_dim, 
             encoder_dropout_p=encoder_dropout_p
         )
+    elif architecture == 'audio':
+        model.encoder = AudioConformerEncoder(
+            front_dim = front_dim,
+            encoder_n_layer=encoder_n_layer, 
+            encoder_d_model=encoder_d_model,
+            encoder_n_head=encoder_n_head, 
+            encoder_ff_dim=encoder_ff_dim, 
+            encoder_dropout_p=encoder_dropout_p
+        )
+    elif architecture == 'video':
+        model.encoder = VisualConformerEncoder(
+            front_dim = front_dim,
+            encoder_n_layer=encoder_n_layer, 
+            encoder_d_model=encoder_d_model,
+            encoder_n_head=encoder_n_head, 
+            encoder_ff_dim=encoder_ff_dim, 
+            encoder_dropout_p=encoder_dropout_p
+        )
+
+    # Define Decoder
+    if loss_fn == 'hybrid':
         model.decoder = HybridDecoder(
             vocab_size = vocab_size,
             decoder_n_layer=decoder_n_layer, 
@@ -45,16 +85,7 @@ def build_model(
             decoder_ff_dim=decoder_ff_dim, 
             decoder_dropout_p=decoder_dropout_p
         )
-    elif architecture == 'att' or architecture=='attention':
-        model = AttentionModel(vocab_size=vocab_size, pad_id=pad_id)
-        model.embedder = nn.Linear(vocab_size, decoder_d_model)
-        model.encoder = FusionConformerEncoder(
-            encoder_n_layer=encoder_n_layer, 
-            encoder_d_model=encoder_d_model,
-            encoder_n_head=encoder_n_head, 
-            encoder_ff_dim=encoder_ff_dim, 
-            encoder_dropout_p=encoder_dropout_p
-        )
+    elif loss_fn == 'att' or architecture=='attention':
         model.decoder = TransformerDecoder(
             vocab_size = vocab_size,
             decoder_n_layer=decoder_n_layer, 
@@ -63,21 +94,14 @@ def build_model(
             decoder_ff_dim=decoder_ff_dim, 
             decoder_dropout_p=decoder_dropout_p
         )
-    elif architecture == 'ctc':
-        model = CTCModel(vocab_size=vocab_size, pad_id=pad_id)
-        model.embedder = nn.Linear(vocab_size, decoder_d_model)
-        model.encoder = FusionConformerEncoder(
-            encoder_n_layer=encoder_n_layer, 
-            encoder_d_model=encoder_d_model,
-            encoder_n_head=encoder_n_head, 
-            encoder_ff_dim=encoder_ff_dim, 
-            encoder_dropout_p=encoder_dropout_p
-        )
+    elif loss_fn == 'ctc':
         model.decoder = LinearDecoder(
             vocab_size = vocab_size, 
             decoder_d_model=decoder_d_model,
         )
-
-    print(f"# of total parameters : {count_parameters(model)}")
+        
+    if rank==0 or rank is None:
+        print("Build complete.")
+        print(f"# of total parameters : {count_parameters(model)}")
 
     return model
--- avsr/models/encoder.py
+++ avsr/models/encoder.py
@@ -10,6 +10,7 @@ from avsr.models.resnet.resnet import Resnet1D_front, Resnet2D_front
 class FusionConformerEncoder(nn.Module):
     def __init__(
         self,
+        front_dim : int,
         encoder_n_layer : int, 
         encoder_d_model : int, 
         encoder_n_head : int, 
@@ -18,6 +19,7 @@ class FusionConformerEncoder(nn.Module):
     ):
         super().__init__()
         self.audio_model = AudioConformerEncoder(
+            front_dim = front_dim,
             encoder_n_layer=encoder_n_layer, 
             encoder_d_model=encoder_d_model,
             encoder_n_head=encoder_n_head, 
@@ -25,18 +27,14 @@ class FusionConformerEncoder(nn.Module):
             encoder_dropout_p=encoder_dropout_p
         )
         self.visual_model = VisualConformerEncoder(
+            front_dim = front_dim,
             encoder_n_layer=encoder_n_layer, 
             encoder_d_model=encoder_d_model,
             encoder_n_head=encoder_n_head, 
             encoder_ff_dim=encoder_ff_dim, 
             encoder_dropout_p=encoder_dropout_p
         )
-        self.MLP = nn.Sequential(
-            nn.Linear(encoder_d_model*2, encoder_d_model*4),
-            nn.BatchNorm1d(encoder_d_model*4),
-            nn.ReLU(),
-            nn.Linear(encoder_d_model*4, encoder_d_model)
-        )
+        self.MLP = MLPLayer(encoder_d_model * 2, encoder_d_model)
         
     def forward(self, 
                 video_inputs, video_input_lengths,
@@ -46,7 +44,6 @@ class FusionConformerEncoder(nn.Module):
         visual_feature = self.visual_model(video_inputs, video_input_lengths, None, None)
         features = self.fusion(visual_feature, audio_feature)
         batch_seq_size = features.shape[:2]
-        features = torch.flatten(features, end_dim=1)
         outputs = self.MLP(features)
         outputs = outputs.view(*batch_seq_size, -1)                
         return outputs
@@ -72,6 +69,7 @@ class FusionConformerEncoder(nn.Module):
 class AudioConformerEncoder(nn.Module):
     def __init__(
         self,
+        front_dim : int,
         encoder_n_layer : int, 
         encoder_d_model : int, 
         encoder_n_head : int, 
@@ -79,7 +77,7 @@ class AudioConformerEncoder(nn.Module):
         encoder_dropout_p : float,
     ):
         super().__init__()
-        self.front = Resnet1D_front(1, encoder_d_model)
+        self.front = Resnet1D_front(1, front_dim,)
         self.back  = Conformer_back(
             encoder_n_layer=encoder_n_layer, 
             encoder_d_model=encoder_d_model,
@@ -87,6 +85,7 @@ class AudioConformerEncoder(nn.Module):
             encoder_ff_dim=encoder_ff_dim, 
             encoder_dropout_p=encoder_dropout_p
         )
+        self.projection = nn.Linear(front_dim, encoder_d_model)
         
     def forward(
         self, 
@@ -95,7 +94,8 @@ class AudioConformerEncoder(nn.Module):
         *args, **kwargs
     ):
         outputs = self.front(audio_inputs)
-        outputs = outputs.permute(0,2,1)
+        outputs = outputs.permute(0,2,1) # (B, L, C)
+        outputs = self.projection(outputs)
         outputs = self.back(outputs, audio_input_lengths)
         return outputs
 
@@ -103,6 +103,7 @@ class AudioConformerEncoder(nn.Module):
 class VisualConformerEncoder(nn.Module):
     def __init__(
         self,
+        front_dim : int,
         encoder_n_layer : int, 
         encoder_d_model : int, 
         encoder_n_head : int, 
@@ -110,7 +111,7 @@ class VisualConformerEncoder(nn.Module):
         encoder_dropout_p : float,
     ):
         super().__init__()
-        self.front = Resnet2D_front(3, encoder_d_model)
+        self.front = Resnet2D_front(3, front_dim,)
         self.back  = Conformer_back(
             encoder_n_layer=encoder_n_layer, 
             encoder_d_model=encoder_d_model,
@@ -118,6 +119,7 @@ class VisualConformerEncoder(nn.Module):
             encoder_ff_dim=encoder_ff_dim, 
             encoder_dropout_p=encoder_dropout_p
         )
+        self.projection = nn.Linear(front_dim, encoder_d_model)
         
     def forward(
         self, 
@@ -127,5 +129,24 @@ class VisualConformerEncoder(nn.Module):
     ):
         #outputs = self.front(inputs)
         outputs = video_inputs
+        outputs = self.projection(outputs)
         outputs = self.back(outputs, video_input_lengths)
         return outputs
+        
+        
+class MLPLayer(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super().__init__()
+        self.linear_1 = nn.Linear(input_dim, output_dim*4)
+        self.linear_2 = nn.Linear(output_dim*4, output_dim)
+        self.batchnorm = nn.BatchNorm1d(output_dim * 4)
+        self.relu = nn.ReLU()
+        
+    def forward(self, x):
+        output = self.linear_1(x)
+        output = output.permute((0,2,1))
+        output = self.batchnorm(output)
+        output = output.permute((0,2,1))
+        output = self.relu(output)
+        output = self.linear_2(output)
+        return output

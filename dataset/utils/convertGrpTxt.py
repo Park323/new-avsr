43bfdb70436347123ecbf2701b2bbffb2b9c86fa
Park323 <park32323@gmail.com>
2022-07-05 13:50:51 +0900

    fix

--- dataset/utils/convertGrpTxt.py
+++ dataset/utils/convertGrpTxt.py
@@ -18,12 +18,17 @@ def findGraphemeTokens(text):
 def convertText(path, opt='grp2char'):
     with open(path, 'r') as f:
         texts = f.readlines()
+    
+    # split units
     results = []
     for i in range(0,len(texts),3):
         results.append(texts[i:i+3])
+        
+    # sort by target
     results = sorted(results, key=lambda x: x[0])
     
     with open(path.replace('.txt','_c.txt'), 'w', encoding='utf-8') as f:
-        for text in texts:
-            tokens = findGraphemeTokens(text)
-            f.write(grp2char(tokens))
+        for result in results:
+            for text in result:
+                tokens = findGraphemeTokens(text)
+                f.write(grp2char(tokens))

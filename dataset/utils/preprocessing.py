cfae7c190f852a6480b469142d683447c8ea0ffb
Park323 <park32323@gmail.com>
2022-07-06 09:56:20 +0900

    limit validation/test sequence length - fast validation - still enable to infer long sequence if needed.

--- dataset/utils/preprocessing.py
+++ dataset/utils/preprocessing.py
@@ -3,8 +3,11 @@ from dataset.utils.match_split_files import match
 from dataset.utils.sort_labels import sort_it
 from dataset.utils.explore_dataset import *
 
-def gogo(path, split):
+def gogo(path, split, limit = None):
+    # limit : length limit for test/validation data
     path = generate_script_from_path(path, unit='grapheme')
     match(path, split)
     sort_it(path.replace(".txt", f"_Train.txt"))
+    sort_it(path.replace(".txt", f"_Valid.txt"), limit, True)
+    sort_it(path.replace(".txt", f"_Test.txt"), limit, True)
     get_file_size(path.replace(".txt", f"_Train.txt"),path.replace(".txt", f"_Train_max.txt"),limit=16)
--- dataset/utils/sort_labels.py
+++ dataset/utils/sort_labels.py
@@ -1,10 +1,24 @@
+import random
+
 def _token_length(x):
     return len(x.split('\t')[-1].split())
 
-def sort_it(Path:str)->None:
+def check_limit(x, l):
+    if _token_length(x) <= l:
+        return True
+    else:
+        return False 
+
+def sort_it(Path:str, Limit:int = None, shuffle = False)->None:
     with open(Path, 'r', encoding='utf8') as f:
         labels = f.readlines()
+    
     labels = sorted(labels, key=_token_length)
+    if Limit is not None:
+        labels = list(filter(lambda x : check_limit(x, Limit), labels))
+    if shuffle:
+        random.shuffle(labels)
+    
     with open(Path.replace('.txt','_sorted.txt'), 'w', encoding='utf8') as f:
         for label in labels:
             f.write(label)
